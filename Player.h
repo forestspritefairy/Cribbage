0c956009b0a733762a5e29a0e47e39dcce669e63
Thrinador <benjiclark@me.com>
2017-06-25 10:39:18 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,17 +1,28 @@
+#pragma once
 #include "Deck.h"
 
 class Player {
 
 public:
-	virtual int playCard(int turn) = 0;
-	int getScore() { return score; }
-	virtual vector<Card> getCribCards(bool turn) = 0;
-	int calculateHandScore(Card cut);
-	void resetHand(vector<Card> newHand);
-	void addScore(int newScore) { score += newScore; }
-private:
-	int score;
-	vector<Card> holdingHand;
-	vector<Card> playingHand;
-};
+    virtual int playCard(int turn) = 0;
+    virtual vector<Card> getCribCards(bool turn) = 0;
 
+    int getScore() { return score; }
+    int calculateHandScore(Card cut);
+    void resetHand(vector<Card> *newHand);
+    void addScore(int newScore) { score += newScore; }
+    void printCards(vector<Card> v);
+
+    vector<Card> holdingHand;
+    vector<Card> playingHand;
+
+private:
+    int score15(Card cut);
+    int scoreRuns(Card cut);
+    int scoreOfAKind(Card cut);
+    int scoreNubs(Card cut);
+    int find15(vector<Card> numbers, int index, int sum, int score);
+
+    int score;
+    
+};
