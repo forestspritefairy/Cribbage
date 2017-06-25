1d59aec217fee677648ba7ef4caaf06abc8bf3c0
Thrinador <benjiclark@me.com>
2017-06-25 10:36:34 -0700

    Update Deck.cpp
    Fixed a couple of bugs dealing with draw.

--- Deck.cpp
+++ Deck.cpp
@@ -1,32 +1,32 @@
+#pragma once
 #include "Deck.h"
 
-
 Deck::Deck() {
-	for (int value = 1; value <= MAX_DECK_VALUE; value++) {
-		for (int suit = 0; suit < MAX_SUIT_VALUE; suit++) {
-			Card c(value, suit);
-			cards.push_back(c);
-		}
-	}
+    for (int id = 1; id <= MAX_DECK_VALUE; id++) {
+        for (int suit = 0; suit < MAX_SUIT_VALUE; suit++) {
+            Card c(id, suit);
+            cards.push_back(c);
+        }
+    }
 }
 
 Card Deck::draw() {
-	int random = rand() % (AMOUNT_OF_CARDS - 1);
-	Card draw = cards.at(random);
-	cards.erase(cards.begin() + (random - 1));
-	return draw;
+    int random = rand() % (cards.size() - 1);
+    Card draw = cards.at(random);
+    cards.erase(cards.begin() + (random - 1));
+    return draw;
 }
 
 Card Deck::cut() {
-	return cards.at(rand() % (AMOUNT_OF_CARDS - 1));
+    return cards.at(rand() % (cards.size() - 1));
 }
 
 void Deck::resetDeck() {
-	cards.clear();
-	for (int value = 1; value <= MAX_DECK_VALUE; value++) {
-		for (int suit = 0; suit < MAX_SUIT_VALUE; suit++) {
-			Card c(value, suit);
-			cards.push_back(c);
-		}
-	}
+    cards.clear();
+    for (int value = 1; value <= MAX_DECK_VALUE; value++) {
+        for (int suit = 0; suit < MAX_SUIT_VALUE; suit++) {
+            Card c(value, suit);
+            cards.push_back(c);
+        }
+    }
 }
